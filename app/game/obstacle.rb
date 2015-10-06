6a1b9c6c8e06e1fc6b29ee60a91f2b47629352c1
Lukas Elmer <lukas.elmer@gmail.com>
2015-10-06 23:19:15 +0100

    relative object position

--- app/game/obstacle.rb
+++ app/game/obstacle.rb
@@ -6,8 +6,15 @@ class Obstacle
 
   # @param [GameTime] game_time
   # @param [Fixnum] height
-  def initialize(game_time, height)
+  def initialize(game_time, height, duration_to_reach_left_side)
+    @game_time = game_time
     self.height = height
-    self.start_m = game_time.m
+    self.start_m = @game_time.m
+    @duration_to_reach_left_side = duration_to_reach_left_side
+  end
+
+  def relative_position
+    duration_until_now = @game_time.m - self.start_m
+    1.0 - (duration_until_now.to_f / @duration_to_reach_left_side )
   end
 end
--- spec/obstacle_spec.rb
+++ spec/obstacle_spec.rb
@@ -4,7 +4,7 @@ require_relative '../app/helpers/gosu_helper'
 RSpec.describe Score do
   before(:each) do
     @game_time = GameTime.new(327)
-    @obstacle = Obstacle.new(@game_time, 130)
+    @obstacle = Obstacle.new(@game_time, 130, 3000)
   end
 
   describe '#height' do
@@ -18,6 +18,20 @@ RSpec.describe Score do
       expect(@obstacle.start_m).to eq(327)
       @game_time.m = 400
       expect(@obstacle.start_m).to eq(327)
+    end
+  end
+
+  describe '#relative_position' do
+    it 'is moves from left to right' do
+      expect(@obstacle.relative_position).to be_within(0.001).of(1.0)
+      @game_time.m = 1327
+      expect(@obstacle.relative_position).to be_within(0.01).of(0.6666)
+      @game_time.m = 2327
+      expect(@obstacle.relative_position).to be_within(0.01).of(0.3333)
+      @game_time.m = 3327
+      expect(@obstacle.relative_position).to be_within(0.01).of(0.0)
+      @game_time.m = 4327
+      expect(@obstacle.relative_position).to be_within(0.01).of(-0.3333)
     end
   end
 end
